7eeba1ef3fa30542ffb69934b6b834a8ad5956c4
yo <ya90312@gmail.com>
2024-02-27 17:27:29 +0800

    新增sidebar

---
+++ resources/views/component/sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="sidebar float-left bg-zinc-400	w-1/5 h-screen">
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+    </ul>
+</div>
